6fefdc9c40c69bdf32504e89c0ca5dcb13e93445
kasperboling <kasper.boling@gmail.com>
2023-10-04 15:05:05 +0300

    1.4: course information step4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,26 +4,28 @@ import Content from './Content';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content content={part1.name} />
-      <Content content={part2.name} />
-      <Content content={part3.name} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content content={parts[0].name} />
+      <Content content={parts[1].name} />
+      <Content content={parts[2].name} />
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </>
   )
 }
