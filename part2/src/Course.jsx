04a7c5458c25f8712040deed22202afe8bf37f56
kasperboling <kasper.boling@gmail.com>
2023-10-05 15:41:18 +0300

    2.2: Course information step7

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -1,8 +1,11 @@
 const Course = ({ course }) => {
+    const total = course.parts.reduce((acc, curr) => acc += curr.exercises, 0)
+
   return (
     <div>
         <h1>{course.name}</h1>
         {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+        <p>Total of {total} exercises</p>
     </div>
   )
 }
