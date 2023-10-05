f82c66e321942173db7423bcf37d4f069ed9699f
kasperboling <kasper.boling@gmail.com>
2023-10-05 10:43:39 +0300

    1.12*: anecdotes step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,68 +1,24 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.onClick}>{props.label}</button>
-)
-
-const StatisticLine = (props) => (
-  <p>{props.text} {props.value}</p>
-)
-
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad
-  const average = all / 3
-  const positive = (props.good / all) * 100
-
-  if (!all) return <p>No feedback given</p>
-
-  return(
-    <>
-      <h2>Statistics</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th align='left'><StatisticLine text="good"/></th>
-            <th><StatisticLine value={props.good}/></th>
-          </tr>
-          <tr>
-            <th align='left'><StatisticLine text="Neutral"/></th>
-            <th><StatisticLine value={props.neutral}/></th>
-          </tr>
-          <tr>
-            <th align='left'><StatisticLine text="Bad"/></th>
-            <th><StatisticLine value={props.bad}/></th>
-          </tr>
-          <tr>
-            <th align='left'><StatisticLine text="All"/></th>
-            <th><StatisticLine value={all}/></th>
-          </tr>
-          <tr>
-            <th align='left'><StatisticLine text="Average"/></th>
-            <th><StatisticLine value={average.toFixed(3)}/></th>
-          </tr>
-          <tr>
-            <th align='left'><StatisticLine text="Positive"/></th>
-            <th><StatisticLine value={positive.toFixed(0)}/></th>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
 
   return (
     <>
-      <h1>Give Feedback</h1>
-      <Button onClick={() => setGood(good + 1)} label="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} label="neutral" />
-      <Button onClick={() => setBad(bad + 1)} label="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[selected]}
+      <br />
+      <button onClick={() => {setSelected(Math.floor(Math.random() * 9))}}>Next anecdotes</button>
     </>
   )
 }
