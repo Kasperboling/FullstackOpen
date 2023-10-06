73952e27a9609e4464d3313673f315a9372fd2be
kasperboling <kasper.boling@gmail.com>
2023-10-06 12:45:32 +0300

    2.7: The Phonebook Step2

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -6,7 +6,11 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
-
+    const personExists = persons.find(person => person.name === newName);
+    if(personExists !== undefined) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
     const newPerson = { name: newName };
 
     setPersons([...persons, newPerson]);
