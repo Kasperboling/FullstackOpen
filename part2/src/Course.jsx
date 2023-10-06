bf92545538b436b8bb5056e2eee0ce436634d979
kasperboling <kasper.boling@gmail.com>
2023-10-06 09:30:30 +0300

    2.4: Course information step9

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -1,12 +1,21 @@
-const Course = ({ course }) => {
-    const total = course.parts.reduce((acc, curr) => acc += curr.exercises, 0)
-
+const Course = ({ courses }) => {
+    const reduce = (key) => {
+        return (
+            <p>{courses[key].parts.reduce((acc, curr) => acc += curr.exercises, 0)}</p>
+        )
+    }
   return (
-    <div>
-        <h1>{course.name}</h1>
-        {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-        <p>Total of {total} exercises</p>
-    </div>
+    <>
+        {courses && courses.map((course, index) =>
+            <div key={course.id}>
+                <h1>{course.name}</h1>
+                    {course.parts.map(part =>
+                        <p key={part.id}>{part.name} {part.exercises}</p>
+                    )}
+                {reduce(index)}
+            </div>
+        )}
+    </>
   )
 }
 
